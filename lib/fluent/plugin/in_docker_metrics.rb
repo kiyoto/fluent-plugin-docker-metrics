752bcd9890306f4260c6427d3baf6cca9ef84db4
Keisuke Ogaki <hikingko1@gmail.com>
2015-08-10 21:37:55 +0900

    fetch basename from container_inspect API

--- lib/fluent/plugin/in_docker_metrics.rb
+++ lib/fluent/plugin/in_docker_metrics.rb
@@ -48,7 +48,7 @@ module Fluent
 
     def list_container_ids
       Docker::Container.all.map do |container|
-        [container.id, container.info["Names"].first]
+        [container.id, container.json["Name"]]
       end
     end
 
